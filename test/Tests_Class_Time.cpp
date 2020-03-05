48e0fb608bebf839c21bea77e17aa375293eecb4
James-Bolt <gleb.frolow2013@gmail.com>
2020-03-05 16:15:25 +0300

    Ispravlenie

--- test/Tests_Class_Time.cpp
+++ test/Tests_Class_Time.cpp
@@ -1,45 +1,40 @@
 #include <gtest/gtest.h>
 #include "Class_Time.hpp"
 
-TEST(Addition, IntToStr) {
+TEST(Class_time, IntToStr) {
   Time T1(0,5,7);
   string T = "00:05:07";
   EXPECT_EQ(T1.IntToStr(),T);
 }
 
-TEST(Addition, OperatorMinus) {
+TEST(Class_time, OperatorMinus) {
   Time T1(15, 33, 23);
   Time T2(3, 23, 10);
   Time T3(12, 10, 13);
-  Time T4 = T1 - T2;
-  EXPECT_EQ(T4.IntToStr(), T3.IntToStr());
+  EXPECT_EQ(T1 - T2, T3);
 }
 
-TEST(Addition, OperatorPlus) {
+TEST(Class_time, OperatorPlus) {
   Time T1(15, 59, 23);
   Time T2(3, 23, 10);
   Time T3(19, 22, 33);
-  Time T4 = T1 + T2;
-  EXPECT_EQ(T4.IntToStr(), T3.IntToStr());
+  EXPECT_EQ(T1 + T2, T3);
 }
 
-TEST(Addition, OperatorGreater) {
+TEST(Class_time, OperatorGreater) {
   Time T1(15, 33, 23);
   Time T2(3, 23, 10);
-  int a = T1 > T2;
-  EXPECT_EQ(a, 1);
+  EXPECT_EQ(T1 > T2, 1);
 }
 
-TEST(Addition, OperatorLess) {
+TEST(Class_time, OperatorLess) {
   Time T1(15, 33, 23);
   Time T2(3, 23, 10);
-  int a = T1 < T2;
-  EXPECT_EQ(a, 0);
+  EXPECT_EQ(T1 < T2, 0);
 }
 
-TEST(Addition, OperatorEquality) {
+TEST(Class_time, OperatorEquality) {
   Time T1(3, 23, 10);
   Time T2(3, 23, 10);
-  int a = T1 == T2;
-  EXPECT_EQ(a, 1);
+  EXPECT_EQ(T1 == T2, 1);
 }
