fd3752eacc4e1595d0cd5b5b7f2f24fb62d54e3d
Diana <diana.ord@icloud.com>
2020-03-24 23:35:40 +0300

    Realisation

--- include/dict.h
+++ include/dict.h
@@ -11,6 +11,7 @@ class Dict { //слово-перевод
 public:
   Dict();
   Dict(std::string a, std::string b);
+  void OB(std::string a);
   string getW();
   string getT();
   friend string getW(Dict a);
@@ -20,35 +21,8 @@ public:
   void correct();
   friend ostream& operator << (ostream& stream, const Dict& a);
   friend istream& operator >> (istream& stream, Dict& a);
+  ~Dict();
 private:
   std::string word, translation;
 };
-
-class ALLDict{ // несколько 
-public:
-  ALLDict();
-  ALLDict(int i);
-  ALLDict(Dict a);
-  ALLDict(Dict a, Dict b);
-  ALLDict operator+(const ALLDict& t);
-  ALLDict& operator=(const ALLDict& t);
-  friend ostream& operator << (ostream& stream, const ALLDict& a);
-  friend istream& operator >> (istream& stream, ALLDict& a);
-  friend bool operator==(const  ALLDict& left, const  ALLDict& right) {
-    int a=0;
-    if (left.size == right.size) {
-      for (int i = 0; i < left.size; i++) {
-        if (left.ALLD[i].getW() == right.ALLD[i].getW())
-          if (left.ALLD[i].getT() == right.ALLD[i].getT()) {
-            a++;
-          } else return 0;
-      }
-    }
-    return 1;
-  };
-  ~ALLDict();
-private:
-  Dict * ALLD;
-  int size;
-};
 #endif  // INCLUDE_ADD_H_
