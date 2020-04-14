6604ed993e3a8d7163c3f0151725d332f333f576
MarssBoot <whitenoize2001@yandex.ru>
2020-04-14 14:57:54 +0300

    Octal

--- test/test_Octal.cpp
+++ test/test_Octal.cpp
@@ -1,70 +1,84 @@
 #include <gtest/gtest.h>
 #include "Octal.h"
 
-TEST(Addition, CanAddTwoPositiveNumbers) {
+TEST(Addition, CanAddTwoPositiveNumbersOneLenght) {
   Octal a = "26574";
   Octal b = "37252";
   Octal c = a + b;
   ASSERT_EQ(c, "66046");
 }
-TEST(Addition, CanAddTwoPositiveNumbers2) {
+TEST(Addition, CanAddTwoPositiveNumbersWhenTheFirstChangeOfTheOther) {
   Octal a = "37252";
   Octal b = "26574";
   Octal c = a + b;
   ASSERT_EQ(c, "66046");
 }
-TEST(Addition, CanAddTwoPositiveNumbers3) {
-  Octal a = "7";
+TEST(Addition, CanAddTwoNumbersDifferentLenghtFirstLong) {
+  Octal a = "17";
   Octal b = "1";
   Octal c = a + b;
-  ASSERT_EQ(c, "10");
+  ASSERT_EQ(c, "20");
 }
-TEST(Addition, CanAddTwoPositiveNumbers4) {
-  Octal a = "6";
-  Octal b = "1";
+TEST(Addition, CanAddTwoNumbersDifferentLenghtSecondLong) {
+  Octal a = "1";
+  Octal b = "17";
   Octal c = a + b;
-  ASSERT_EQ(c, "7");
+  ASSERT_EQ(c, "20");
 }
-TEST(raznost, CanDiffTwoPositiveNumbers) {
+TEST(raznost, CanDiffTwoNumbersDifferentLenghtFirstLong) {
   Octal a = "23745";
   Octal b = "3653";
   Octal c = a - b;
   ASSERT_EQ(c, "20072");
 }
 
-TEST(raznost, CanDiffTwoPositiveNumbers2) {
-  Octal a = "32";
-  Octal b = "63";
+TEST(raznost, CanDiffTwoNumbersOneLenght) {
+  Octal a = "63";
+  Octal b = "32";
   Octal c = a - b;
   ASSERT_EQ(c, "31");
 }
-TEST(raznost, CanDiffTwoPositiveNumbers3) {
-  Octal a = "10";
-  Octal b = "1";
+TEST(raznost, CanDiffTwoNumbersDifferentLenghtSecondLong) {
+  Octal a = "23";
+  Octal b = "162";
   Octal c = a - b;
-  ASSERT_EQ(c, "7");
+  ASSERT_EQ(c, "137");
 }
-TEST(raznost, CanDiffTwoPositiveNumbers4) {
-  Octal a = "7";
-  Octal b = "1";
-  Octal c = a - b;
-  ASSERT_EQ(c, "6");
-}
-TEST(sravnenie, CanSravnTwoPositiveNumbers) {
+TEST(sravnenie, CanSravnTwoPositiveNumbersRavni) {
   Octal a = "25";
   Octal b = "25";
-  Octal p1 = "1";
+  bool k=0;
   if (a == b) {
-   ASSERT_EQ(p1, "1");
+    k = 1;
   } 
+  ASSERT_EQ(k, 1);
 }
-TEST(sravnenie, CanSravnTwoPositiveNumbers2) {
+TEST(sravnenie, CanSravnTwoPositiveNumbersRavniPomenyalMestami) {
+  Octal a = "25";
+  Octal b = "25";
+  bool k = 0;
+  if (b == a) {
+    k = 1;
+  }
+  ASSERT_EQ(k, 1);
+}
+TEST(sravnenie, CanSravnTwoPositiveNumbersNeRavni) {
   Octal a = "24";
   Octal b = "25";
-  Octal p1 = "0";
+  bool k = 0;
   if (a == b) {
-    ASSERT_EQ(p1, "0");
+     k = 1;
   }
+  ASSERT_EQ(k, 0);
+}
+TEST(sravnenie, CanSravnTwoPositiveNumbersNeRavniPomenyalMestami) {
+  Octal a = "24";
+  Octal b = "25";
+  bool k = 0;
+  if (a == b) {
+    k = 1;
+  }
+  ASSERT_EQ(k, 0);
 }
 TEST(priravniaem, CanpriravnivaemTwoPositiveNumbers) {
   Octal a = "40";
