b5724fa4e9e8db444733d1d68c4b552bc2a5b188
Khazker <60774094+Khazker@users.noreply.github.com>
2020-05-27 23:01:16 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,17 @@
+<<<<<<< Updated upstream
 #include "add.h"
 #include <iostream>
 
 int main() {
   std::cout << "5 + 5 = " << add(5, 5) << std::endl;
 }
+=======
+#include "Snake.h"
+
+using namespace std;
+
+int main() {
+  Zmeika z;
+  z.startmenu();
+}
+>>>>>>> Stashed changes
