1ff382c63ffbecea23b4d09f80640bdf26fbf826
Khazker <60774094+Khazker@users.noreply.github.com>
2020-05-27 16:10:44 +0300

    Update Pedometr.h

--- include/Pedometr.h
+++ include/Pedometr.h
@@ -1,3 +1,9 @@
+<<<<<<< Updated upstream
+=======
+#ifndef INCLUDE_Pedometr_H_
+#define INCLUDE_Pedometr_H_
+
+>>>>>>> Stashed changes
 #pragma once
 #include <fstream>
 
@@ -13,17 +19,25 @@ private:
 public:
   Time();
   Time(int* _data, int* _tSt, int* _tFn, int _steps);
+<<<<<<< Updated upstream
   ~Time();
 
   friend std::ostream& operator<<(std::ostream& stream, const Pedometr& s);
   friend std::istream& operator>>(std::istream& stream, Time& s);
 
   void info();
+=======
+  Time(const Time& t);
+  ~Time();
+
+  friend class Pedometr;
+>>>>>>> Stashed changes
 };
 
 class Pedometr {
 private:
   Time* time;
+<<<<<<< Updated upstream
 
 public:
   Pedometr();
@@ -38,10 +52,30 @@ public:
   void getData();
   void startInfo();
   void addCount();
+=======
+  int num;
+
+public:
+  Pedometr();
+  Pedometr(int* _data, int* _tSt, int* _tFn, int _steps);
+  Pedometr(const Pedometr& p);
+  ~Pedometr();
+
+  void menu();
+  void newRecord();
+  void read();
+  void getStartTime();
+>>>>>>> Stashed changes
   void getInfoEpisode();
   void middleMounth();
   void middleAll();
   void maxStepDay();
   void maxStepAll();
 
+<<<<<<< Updated upstream
 };
+=======
+};
+
+#endif  // INCLUDE_Pedometr_H_
+>>>>>>> Stashed changes
